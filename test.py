a22a077b211af874b2b13791c463764d1380838a
altivelis <ayupon89821026@gmail.com>
2023-07-21 19:18:38 +0900

    a

---
+++ test.py
@@ -0,0 +1,14 @@
+import requests
+import dotenv
+dotenv.load_dotenv()
+import os
+
+url_status = os.getenv('URL_STATUS')
+url_notify = os.getenv('URL_NOTIFY')
+token = os.getenv('TOKEN')
+
+requests.post(url=url_notify+'81',
+              params={
+                  "message":"テストメッセージ",
+                  "token":token
+              })
